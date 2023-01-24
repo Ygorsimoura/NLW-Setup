a4a60a23ffbd7958b32850b0f22c44ba53e7bd49
Ygor Simoura <ygorsimoura01@gmail.com>
2023-01-24 13:23:09 -0300

    correção de erros no JS

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ button.addEventListener("click", add)
 form.addEventListener("change", save)
 
 function add() {
-  const today = new Date().toLocaleString("pt-br").slice(0, -5)
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
   const dayExists = nlwSetup.dayExists(today)
 
   if (dayExists) {
@@ -18,10 +18,10 @@ function add() {
 }
 
 function save() {
-  localStorage.setItem("NLWstup@habits", JSON.stringify(nlwSetup.data))
+  localStorage.setItem("NLWSetup@habits", JSON.stringify(nlwSetup.data))
 }
 
-const data = JSON.parse(localStorage.getItem("NLWstup@habits")) || {}
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {}
 nlwSetup.setData(data)
 nlwSetup.load()
  
